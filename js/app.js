2df3a5443be454fc32ec8bfe5f5ddcb314d1fa79
mubashirbappi <mubashirbappi@gmail.com>
2024-10-03 20:08:26 +0600

    video card modify

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 console.log("testinggg")
+
+
+//  time function
+
+const getTime = time =>{
+    const hours = parseInt(time/3600)
+    const remainingtimes =time%3600
+    const min =parseInt(remainingtimes/60)
+    remain = remainingtimes%60
+    
+    return `${hours}hrs ${min}min ${remain}sec ago`
+}
+//  categories type
 const loadData = async() => {
    try{
     const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -46,20 +59,42 @@ const displayVideos = (videos) =>{
         card.classList.add("card","card-compact")
         card.innerHTML = `
         
-  <figure class="h-[200px]">
+  <figure class="h-[200px] relative">
     <img
       src="${video.thumbnail}"
       class="w-full h-full object-cover"
       alt="Shoes" />
+      ${video.others.posted_date?.length ===0 ? "" :`<span class="absolute right-2 bottom-2 bg-black text-white p-2 rounded-md">${getTime(video.others.posted_date)} </span>`}
   </figure>
-  <div class="px-0 py-2">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
+  <div class="px-0 py-2  gap-2">
+<div class="flex  items-center gap-4">
+<img
+      src="${video.authors[0].profile_picture}"
+      class="w-10 h-10 object-cover rounded-full"
+      alt="profile">
+ <div>
+     <div>
+         <h2 class="font-bold text-2xl">${video.title} </h2>
+     </div>
+
+     <div class="flex gap-2">
+           <p class="text-gray-400">${video.authors[0].profile_name}</p>
+            ${video.authors[0].verified === true ? ' <img class="w-5" src="https://img.icons8.com/?size=100&id=2AuMnRFVB9b1&format=png&color=000000">': ""}
+     </div>
+    <p class="text-gray-400">${video.others.views}</p>
 </div>
+ 
+
+
+
+</div>
+
+
+   
+    
+    
+  </div>
+
 
         
         
